4f49827bb2a4da97b7e358f375a10d1dde241564
2204101160-48 <panditanu965@gmail.com>
2024-09-06 10:29:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+let sustain=document.querySelector("#sustain");
+sustain.addEventListener("click",function(){
+   window.open("sustain.html","_blank");
+})
+
+let info=document.querySelector("#aboutus");
+info.addEventListener("click",function(){
+   window.open("abtus.html","_blank");
+});
+let info1=document.querySelector("#service");
+info1.addEventListener("click",function(){
+   window.open("services.html","_blank");
+});
+
+
+
